906a332d930e1f1a6f58ee9ba84c356b514f2810
Kevin Tam <tam580@gmail.com>
2017-03-10 23:10:34 -0600

    front end

---
+++ users/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    context = {'name':'kevin', 'tasks': ['do your hw', 'fall in love', 'save the world']}
+
+    return render(request, 'users/index.html', context)
+
+def detail(request):
+    return HttpResponse("<h1>This is the detail view!</h1>")
+
+def add(request):
+    return HttpResponse("<h1>This is the add view!</h1>")
